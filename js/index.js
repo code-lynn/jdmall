9a98c75b516808cea4e0eefbb645bad5bade25e6
code-lynn <lylcode@163.com>
2016-12-22 20:31:08 +0800

    1222

--- js/index.js
+++ js/index.js
@@ -6,6 +6,6 @@ var slide=new Slide({
         id:'#slide',
         url:'json/header/slide.txt',
         duration:1000,
-        interval:2500
+        interval:2000
 //        effect:2 //jQuery的animate中没有这个参数
     });
